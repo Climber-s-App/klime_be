08e73ea5810eed3c282f7562a40cd332dfc794d8
Blaine Glasgow <glasgowb@outlook.com>
2023-08-28 13:16:14 -0700

    Add endpoints for users, user, user walls, user wall details and updated serializers

--- klime/serializers.py
+++ klime/serializers.py
@@ -8,7 +8,49 @@ class WallSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wall
         fields = ["id", "name", "photo_url", "user_id"]
+class WallsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wall
+        fields = ["id", "name", "photo_url", "user_id"]
 class ProblemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Problem
         fields = ["id", "name", "vectors", "wall_id"]
+
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     vectors_data = instance.object.vectors.all()  # Replace this with your actual logic to fetch vectors data
+
+    #     vectors_representation = []
+    #     for vector in vectors_data:
+    #         vector_representation = {
+    #             "id": vector.id,  # Include the id field
+    #             "x": vector.x,    # Include other fields as needed
+    #             "y": vector.y,
+    #             "z": vector.z,
+    #         }
+    #         vectors_representation.append(vector_representation)
+
+    #     representation["vectors"] = vectors_representation
+    #     return representation
+
+        # representation = super().to_representation(instance)
+        # new_representation = {
+        #     'id': representation.get('id'),
+        #     'type': 'library',
+        #     'attributes': {
+        #         'name': representation.get('name'),
+        #         'address': {
+        #             'street': representation.get('street'),
+        #             'city': representation.get('city'),
+        #             'state': representation.get('state'),
+        #             'zip': representation.get('zip'),
+        #         },
+        #         'location': {
+        #             'lat': representation.get('lat'),
+        #             'lon': representation.get('lon'),
+        #         },
+        #         'book_count': instance.book_set.count(),
+        #     }
+        # }
+        # return new_representation
