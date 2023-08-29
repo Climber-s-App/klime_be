0bc4d5f9e729cd16b6ed760d7f361d32b95ca086
Jesse Thomas <jgthomas3@gmail.com>
2023-08-29 17:24:22 -0600

    Adjust settings.py for deploy

--- klime/settings.py
+++ klime/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'klime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'klimedb',       # Name of the PostgreSQL database
+        'USER': 'juser',     # PostgreSQL username
+        'PASSWORD': '',  # PostgreSQL password
+        'HOST': '127.0.0.1',    # PostgreSQL server hostname (usually 'localhost')
+        'PORT': '5432',         # PostgreSQL port (usually 5432)
     }
 }
 
