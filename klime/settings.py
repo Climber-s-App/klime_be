bd904c3b408c3f5bfa25f2e4b109e65bcf99f468
Blaine Glasgow <glasgowb@outlook.com>
2023-08-30 11:07:00 -0700

    added_psycog2

--- klime/settings.py
+++ klime/settings.py
@@ -81,12 +81,23 @@ WSGI_APPLICATION = 'klime.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Feel free to alter this value to suit your needs.
+#         default='postgresql://postgres:postgres@localhost:5432/mysite',
+#         conn_max_age=600
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'klime_be',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '',
+        'PORT': '',
+    }
 }
 
 #use for local testing
